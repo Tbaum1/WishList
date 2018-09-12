e994f4d52b41d5b7fbdf1da886dab9b07babeaa2
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-09-12 18:29:24 -0500

    Add project files.

---
+++ WishList/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Content;
+
+namespace WishList
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        string[] wishList = new string[5];
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);                       
+            wishList[0] = "Porsche";
+            wishList[1] = "Xbox One X";
+            wishList[2] = "Gaming Keyboard";
+            wishList[3] = "Monitor";
+            wishList[4] = "Australian Vacation";
+            
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, wishList);            
+        }
+        
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            base.OnListItemClick(l, v, position, id);
+            Intent secondActivity = new Intent(this, typeof(SecondActivity));
+            string item = wishList[position];
+            Toast.MakeText(this, "They clicked: " + item, ToastLength.Long).Show();
+
+            if (position == 0)
+            {
+                secondActivity.PutExtra("wish","porsche");
+                secondActivity.PutExtra("url", "https://www.porsche.com/usa/models/718/718-gts-models/");
+                StartActivity(secondActivity);
+            }else if (position == 1)
+            {
+                secondActivity.PutExtra("wish", "xbox");
+                secondActivity.PutExtra("url", "https://www.xbox.com/en-US/xbox-one-x?xr=shellnav");
+                StartActivity(secondActivity);
+            }
+            else if (position == 2)
+            {
+                secondActivity.PutExtra("wish", "keyboard");
+                secondActivity.PutExtra("url", "https://www.bestbuy.com/site/razer-blackwidow-chroma-v2-wired-gaming-mechanical-green-switch-keyboard-with-rgb-back-lighting-black/5588907.p?skuId=5588907");
+                StartActivity(secondActivity);
+            }
+            else if (position == 3)
+            {
+                secondActivity.PutExtra("wish", "monitor");
+                secondActivity.PutExtra("url", "https://www.bestbuy.com/site/dell-27-led-qhd-gsync-monitor-black/5293502.p?skuId=5293502");
+                StartActivity(secondActivity);
+            }
+            else if (position == 4)
+            {
+                secondActivity.PutExtra("wish", "australia");
+                secondActivity.PutExtra("url", "https://www.tripadvisor.com/Vacation_Packages-g255066-Darwin_Top_End_Northern_Territory-Vacations.html");
+                StartActivity(secondActivity);
+            }
+        }
+    }
+}
---
+++ WishList/SecondActivity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WishList
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Android.App.Activity
+    {
+        Button btnViewMore, btnGoBack;
+        string wish, url;
+        ImageView imgViewWish;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_second);
+            btnViewMore = FindViewById<Button>(Resource.Id.btnViewMore);
+            btnViewMore.Click += BtnViewMore_Click;
+            wish = Intent.GetStringExtra("wish") ?? "Not Recv";
+            url = Intent.GetStringExtra("url") ?? "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwiQ3LO3prbdAhVVIzQIHdUZDGsQPAgD";
+            imgViewWish = FindViewById<ImageView>(Resource.Id.imgViewWish);
+            btnGoBack = FindViewById<Button>(Resource.Id.btnGoBack);
+            findImage();
+            btnGoBack.Click += delegate
+            {
+                this.Finish();
+            };
+
+        }
+
+        private void findImage()
+        {
+            if (wish == "porsche")
+            {
+                imgViewWish.SetImageResource(Resource.Drawable.porsche);
+            }
+            else if (wish == "xbox")
+            {
+                imgViewWish.SetImageResource(Resource.Drawable.xbox);
+            }
+            else if (wish == "keyboard")
+            {
+                imgViewWish.SetImageResource(Resource.Drawable.keyboard);
+            }
+            else if (wish == "monitor")
+            {
+                imgViewWish.SetImageResource(Resource.Drawable.monitor);
+            }
+            else if (wish == "australia")
+            {
+                imgViewWish.SetImageResource(Resource.Drawable.australia);
+            }
+        }
+
+
+        private void BtnViewMore_Click(object sender, EventArgs e)        
+        {            
+            var uri = Android.Net.Uri.Parse(url);
+            var intent = new Intent(Intent.ActionView, uri);
+            if (wish == "porsche")
+            {                          
+                StartActivity(intent);
+            }
+            else if (wish == "xbox")
+            {
+                StartActivity(intent);
+            }
+            else if (wish == "keyboard")
+            {
+                StartActivity(intent);
+            }
+            else if (wish == "monitor")
+            {
+                StartActivity(intent);
+            }
+            else if (wish == "australia")
+            {
+                StartActivity(intent);
+            }
+
+        }
+
+        
+    }
+    
+}
